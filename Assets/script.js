28912154e4b2a0d020361094016831f6531e9c09
gwarz <gmwarzecha@tutanota.com>
2021-01-04 15:07:41 -0700

    added ecommclearents q : q clear

--- Assets/script.js
+++ Assets/script.js
@@ -8,7 +8,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+// variables for characters to populate the password
 function generatePassword() {
   let passArray  = [];
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -16,26 +16,32 @@ function generatePassword() {
   var nums = ["0","1","2","3","4","5","6","7","8","9"];
   var specialChars = ["!","$","%","&","?"];
 
-  var passLength = prompt("How long would you like your password to be, between 8 and 128 characters"); 
+  var passLength = prompt("Pick a password length between 8 and 128 character."); 
   console.log(passLength);
 
-  
-  if (passLength < 8 || passLength > 128) {
+  // conditional to establish proper password length
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     window.alert("please pick a valid option");
     return "";
   } else {
     window.alert("You have picked a " + passLength + " character-long password");
   }
 
+  // character prompts
+  var lowerConfirm = confirm("Include lower case letters in your password?");
+  var upperConfirm = confirm("Include upper case letters in your password?");
+  var numsConfirm = confirm("Include numbers in your password?");
+  var specialCharsConfirm = confirm("Include special characters in your password?")
 
-  var lowerConfirm = confirm("Would you like lower case letters in your password?");
-  var upperConfirm = confirm("Would you like upper case letter in your password?");
-  var numsConfirm = confirm("Would you like numbers in your password?");
-  var specialCharsConfirm = confirm("Would you like special characters in your password?")
+  if (lowerConfirm, upperConfirm, numsConfirm, specialCharsConfirm === false) {
+    window.alert("Please pick at least one valid character option.");
+    return "";
+  }
 
+  // stores the character criteria that is selected via prompts
   let selectedCriteria = []
 
-
+  // conditionals to decide which characters populate when password is generated
   if (lowerConfirm === true) {
     selectedCriteria.push(...lowerCase);
   }
@@ -54,16 +60,16 @@ function generatePassword() {
 
   for (var i = 0; i < passLength; i++) {
     var number = Math.floor(Math.random() * Math.floor(selectedCriteria.length));
-    var value = selectedCriteria[number];
-    passArray.push(value)
+    var value = selectedCriteria[number]; // assigns the indeces correlated to the number var that were randomly selected from selectedCriteria to the value var
+    passArray.push(value) // pushes the characters assigned to value into the passArray
   }
 
   if (passArray.length > 0) {
-    password = passArray.join('');
+    password = passArray.join(''); // joins the individual array values as a string
     return password;
   }
   
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
