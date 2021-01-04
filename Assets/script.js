da9b335781a525a2df74f071414fe4daf47aff59
gwarz <gmwarzecha@tutanota.com>
2021-01-04 16:00:52 -0700

    fixed grammatical errors

--- Assets/script.js
+++ Assets/script.js
@@ -16,22 +16,22 @@ function generatePassword() {
   var nums = ["0","1","2","3","4","5","6","7","8","9"];
   var specialChars = ["!","$","%","&","?"];
 
-  var passLength = prompt("Pick a password length between 8 and 128 character."); 
+  var passLength = prompt("Pick a password length between 8 and 128 characters."); 
   console.log(passLength);
 
   // conditional to establish proper password length
   if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    window.alert("please pick a valid option");
+    window.alert("Please pick a valid option");
     return "";
   } else {
-    window.alert("You have picked a " + passLength + " character-long password");
+    window.alert("You selected a password of " + passLength + " characters");
   }
 
   // character prompts
   var lowerConfirm = confirm("Include lower case letters in your password?");
   var upperConfirm = confirm("Include upper case letters in your password?");
   var numsConfirm = confirm("Include numbers in your password?");
-  var specialCharsConfirm = confirm("Include special characters in your password?")
+  var specialCharsConfirm = confirm("Include special characters in your password?");
 
   if (lowerConfirm, upperConfirm, numsConfirm, specialCharsConfirm === false) {
     window.alert("Please pick at least one valid character option.");
@@ -61,7 +61,7 @@ function generatePassword() {
   for (var i = 0; i < passLength; i++) {
     var number = Math.floor(Math.random() * Math.floor(selectedCriteria.length));
     var value = selectedCriteria[number]; // assigns the indeces correlated to the number var that were randomly selected from selectedCriteria to the value var
-    passArray.push(value) // pushes the characters assigned to value into the passArray
+    passArray.push(value); // pushes the characters assigned to value into the passArray
   }
 
   if (passArray.length > 0) {
